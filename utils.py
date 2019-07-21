bb2c24d07bf3b554b6e57676329fcdc9cb807e73
basrizk <basem.rizk@outlook.com>
2019-07-21 18:06:22 +0200

    utils adapt for nvidia-smi not working on jetson devices

--- utils.py
+++ utils.py
@@ -21,7 +21,9 @@ def cpu_info():
 def gpu_info():
     if platform.system() == 'Linux':
         command = 'nvidia-smi'
-        return os.popen(command).read().strip()
+        if not os.popen(command).read().strip() == "":
+            return os.popen(command).read().strip()
+        return "Probably running on Jetson NANO with Nvidia MaxWel GPU - Tegra"
     return 'platform not identified'
 
 
